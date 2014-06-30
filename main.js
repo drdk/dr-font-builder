a752fdd209d1f2f719e20990efaf0521ce31dbbb
Rasmus Fløe <rasmusfl0e@gmail.com>
2014-07-01 01:40:57 +0200

    semi-working code

--- main.js
+++ main.js
@@ -62,33 +62,50 @@ function build (config, sourceDir, dest, formats) {
 				throw(err);
 			}
 
-			if (formats.indexOf("ttf") > -1 || (formats.indexOf("woff") > -1 || formats.indexOf("eot") > -1)) {
+			if (formats.indexOf("ttf") > -1 || formats.indexOf("woff") > -1 || formats.indexOf("eot") > -1) {
 
 
-				fs.readFile(svgFile, {encoding: "utf8"}, function (err, data) {
+				fs.readFile(svgFile, {encoding: "utf8"}, function (err, svg) {
 
 					if (err) {
 						throw(err);
 					}
 
 					var svg2ttf = require("svg2ttf");
-
-					var ttf = svg2ttf(data);
+					var ttf = svg2ttf(svg);
 
 					if (formats.indexOf("ttf") > -1) {
-						fs.writeFile(filename + ".ttf", new Buffer(ttf.buffer));
+						var ttfFile = filename + ".ttf";
+						fs.writeFile(ttfFile, new Buffer(ttf.buffer), function (err) {
+							if (err) {
+								throw(err);
+							}
+							console.log("Built:", ttfFile);
+						});
 					}
 
 					if (formats.indexOf("woff") > -1) {
 						var ttf2woff = require("ttf2woff");
-						var woff = ttf2woff(new Uint8Array(ttf));
-						fs.writeFile(filename + ".woff", new Buffer(woff.buffer));
+						var woffFile = filename + ".woff";
+						var woff = ttf2woff(new Uint8Array(ttf.buffer));
+						fs.writeFile(woffFile, new Buffer(woff.buffer), function (err) {
+							if (err) {
+								throw(err);
+							}
+							console.log("Built:", woffFile);
+						});
 					}
 
 					if (formats.indexOf("eot") > -1) {
 						var ttf2eot = require("ttf2eot");
-						var eot = ttf2eot(new Uint8Array(ttf));
-						fs.writeFile(filename + ".eot", new Buffer(eot.buffer));
+						var eotFile = filename + ".eot";
+						var eot = ttf2eot(new Uint8Array(ttf.buffer));
+						fs.writeFile(eotFile, new Buffer(eot.buffer), function (err) {
+							if (err) {
+								throw(err);
+							}
+							console.log("Built:", eotFile);
+						});
 					}
 
 				});
