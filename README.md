3b883e9d55dfc07650f09c7c95b106183f852fd3
rafl <rafl@dr.dk>
2014-09-10 14:49:32 +0200

    added -d option and docs

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 dr-font-builder
 ===============
 
-Work in progress...
+Convert SVG fonts to Truetype, WOFF or EOT formats.
+
+(Basically just a thin wrapper around the brilliant [svg2tff](https://github.com/fontello/svg2ttf), [ttf2woff](https://github.com/fontello/ttf2woff) and [ttf2eot](https://github.com/fontello/ttf2eot) [Fontello](https://github.com/fontello) libraries.)
+
+## Usage
+
+### CLI
+
+```
+font-builder <file> [options] 
+```
+
+#### Options
+
+* `-f, --formats [string]` - A list of formats to convert the source file too. Supported formats: `ttf`, `woff` and `eot`. Default is to export all supported formats.
+* `-d, --dest [string]` - Destination path of the converted files. Default destination is the directory of the source file.
+
+#### Example
+
+```
+font-builder my-font.svg -f ttf,woff
+```
+
+
+### Node
+
+```
+var builder = require("dr-font-builder");
+
+builder("my-font.svg", ["ttf", "woff"]);
+```
--- main.js
+++ main.js
@@ -1,35 +1,48 @@
 var fs = require("fs");
 var path = require("path");
 
-var allowedFormats = ["svg", "ttf", "woff", "eot"];
+var supportedFormats = ["svg", "ttf", "woff", "eot"];
 
 if (require.main === module) {
 	var optimist = require("optimist")
-		.usage("Build fonts from SVG glyphs.\nUsage: font-builder -i [file] -f [string]")
+		.usage("Build fonts from SVG glyphs.\nUsage: $0 [file] -f [string]")
 		//.alias('c', 'config')
-		.alias('i', 'input')
-		//.alias('o', 'output')
+		.alias('d', 'dest')
 		.alias('f', 'formats')
 		.check(function(argv){
-			if (!argv.input) {
-				throw "Input param missing!";
+			if (argv._.length === 0) {
+				throw "Input file missing!";
 			}
 		});
 	var argv = optimist.argv;
-	var formats;
+	var formats = supportedFormats;
 	if (argv.formats) {
 		formats = argv.formats.split(",").filter(function (format) {
-			return (allowedFormats.indexOf(format) > -1);
+			return (supportedFormats.indexOf(format) > -1);
 		});
 	}
-	build(argv.input, formats);
+	build(argv._[0], argv.dest, formats);
 }
 
-function build (source, formats) {
+function build (source, dest, formats) {
 
-	var filename = source.replace(/\.(svg|ttf|woff|eot)$/, "");
+	var filename = path.basename(source, path.extname(source));
 
-	formats = formats || allowedFormats;
+	if (Array.isArray(dest)) {
+		formats = dest;
+		dest = null;
+	}
+
+	if (typeof dest !== "string") {
+		dest = path.dirname(source);
+	}
+
+	if (!fs.existsSync(dest)) {
+		var mkdirp = require("mkdirp");
+		mkdirp.sync(dest);
+	}
+
+	formats = formats || supportedFormats;
 
 	if (formats.indexOf("ttf") > -1 || (formats.indexOf("woff") > -1 || formats.indexOf("eot") > -1)) {
 
@@ -39,19 +52,19 @@ function build (source, formats) {
 		var ttf = svg2ttf(svg);
 
 		if (formats.indexOf("ttf") > -1) {
-			fs.writeFileSync(filename + ".ttf", new Buffer(ttf.buffer));
+			fs.writeFileSync(path.join(dest, filename + ".ttf"), new Buffer(ttf.buffer));
 		}
 
 		if (formats.indexOf("woff") > -1) {
 			var ttf2woff = require("ttf2woff");
 			var woff = ttf2woff(ttf.buffer);
-			fs.writeFileSync(filename + ".woff", new Buffer(woff.buffer));
+			fs.writeFileSync(path.join(dest, filename + ".woff"), new Buffer(woff.buffer));
 		}
 
 		if (formats.indexOf("eot") > -1) {
 			var ttf2eot = require("ttf2eot");
 			var eot = ttf2eot(ttf.buffer);
-			fs.writeFileSync(filename + ".eot", new Buffer(eot.buffer));
+			fs.writeFileSync(path.join(dest, filename + ".eot"), new Buffer(eot.buffer));
 		}
 
 	}
